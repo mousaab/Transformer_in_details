f82c5b0781b3d29bc6bb2c54992cd1b793ccb1fb
mousaab <mousaab.orabi@gmail.com>
2023-09-25 17:25:35 +0200

    add positional wise encoding

---
+++ transformer/positionalencoding.py
@@ -0,0 +1,22 @@
+import math
+
+import torch
+import torch.nn as nn
+from multiheadattention import MultiHeadAttention
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_seq_length):
+        super(PositionalEncoding, self).__init__()
+
+        pe = torch.zeros(max_seq_length, d_model)
+        position = torch.arange(0, max_seq_length, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model))
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        self.register_buffer("pe", pe.unsqueeze(0))
+
+    def forward(self, x):
+        return x + self.pe[:, : x.size(1)]
